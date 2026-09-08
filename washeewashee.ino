e164232947defc7f5e6cb40ac12bcc05d45adda5
goatzillax <goatzillax>
2026-09-08 18:02:10 -0500

    added RTCMem to use reset button as user input

--- washeewashee.ino
+++ washeewashee.ino
@@ -1,6 +1,23 @@
-#include "LittleFS.h"
-
 #include <Arduino.h>
+#include <LittleFS.h>
+
+typedef enum { POWERUP = 0,
+               IDLE = 1,
+               OSCILLATING = 2,
+               SPINNING = 3,
+               LAST = 4 } eWasherState;
+
+#include <RTCMemory.h>
+
+// This is almost like a context struct across resets, could save all kinds of stuff.
+// Max size is 508 bytes.
+typedef struct {
+  eWasherState WasherState;
+} rtcData_struct;
+
+RTCMemory<rtcData_struct> rtcMemory;
+rtcData_struct *rtcData;
+
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
@@ -9,9 +26,7 @@
 unsigned long ota_progress_millis = 0;
 
 void onOTAStart() {
-  // Log when OTA has started
   Serial.println("OTA update started!");
-  // <Add your own code here>
 }
 
 void onOTAProgress(size_t current, size_t final) {
@@ -29,9 +44,7 @@ void onOTAEnd(bool success) {
   } else {
     Serial.println("There was an error during OTA update!");
   }
-  // <Add your own code here>
 }
-
 
 AsyncWebServer server(80);
 
@@ -40,80 +53,91 @@ AsyncWebServer server(80);
 
 int servoHandle = -1;
 
-enum eWasherState { POWERUP,
-                    OSCILLATING,
-                    SPINNING,
-                    IDLE,
-                    LAST };
-
-eWasherState WasherState = POWERUP;
-
 //  probably a more elegant way to do this but whatevs
 unsigned long ts_powerup;
 unsigned long ts_oscillating;
 unsigned long ts_spinning;
+
+#define TIME_POWERDELAY (30 * 1000)
+#define TIME_OSCILLATE (5 * 60 * 1000)
+#define TIME_SPIN (3 * 60 * 1000)
 
 //  this ramp is kind of...  rough.
 int osc_pattern[] = { 135, 150, 180, 180, 180, 120, 90, 45, 30, 0, 0, 0, 45, 90 };
 int osc_pattern_idx;  //  whoever calls for the state change should set these up.
 unsigned long osc_last;
 
+void setWasherState(rtcData_struct *data, eWasherState newstate) {
+  Serial.println("DEBUG:  WasherState = " + String((int)newstate));
+  data->WasherState = newstate;
+  rtcMemory.save();
+}
+
+String processor(const String &var) {
+  if (var == "WASHERSTATE")
+    return (String((int)rtcData->WasherState));
+  return String();
+}
+
+
 void setup() {
-  ts_powerup = millis();
+  ts_powerup = ts_oscillating = ts_spinning = millis();
   Serial.begin(115200);
+
+  Serial.println("\n\n");
+
+  if (rtcMemory.begin()) {
+    Serial.println("INFO:  Initialization done! Previous data found.");
+    rtcData = rtcMemory.getData();
+    rtcData->WasherState = static_cast<eWasherState>(rtcData->WasherState + 1);  // mang fuck dis language
+    if (rtcData->WasherState >= LAST) {
+      rtcData->WasherState = IDLE;
+    }
+  } else {
+    Serial.println("INFO:  Initialization done! No previous data found. The buffer is cleared.");
+    //  dunno what "the buffer is cleared" actually means.
+    rtcData = rtcMemory.getData();
+    rtcData->WasherState = POWERUP;
+  }
+
+  rtcMemory.save();
 
   WiFi.softAP("washeewashee", "washeewashee");
   IPAddress IP = WiFi.softAPIP();
 
-  while (!Serial)
-    ;
-  Serial.println("\n\n");
   Serial.print("AP IP address: ");
   Serial.println(IP);
+  Serial.println("DEBUG:  WasherState = " + String((int)rtcData->WasherState));
 
   if (!LittleFS.begin()) {
     Serial.println("ERROR:  could not initialize LittleFS");
-    return;
+    //return;
   }
-
-#if 0
-        File file = LittleFS.open("/index.html", "r");
-        if(!file){
-                Serial.println("ERROR:  Failed to open index.html for reading");
-                return;
-        }
-  
-        Serial.println("File Content:");
-                while(file.available()){
-                Serial.write(file.read());
-        }
-        file.close();
-#endif
 
   servoHandle = ISR_Servo.setupServo(D4, 1000, 2000);
   if (servoHandle == -1) {
     Serial.println("ERROR:  could not initialize ISR servo");
-  }  //  halt n catch a stray?
-  ISR_Servo.setPosition(servoHandle, 90);
+  }                                        //  halt n catch a stray?
+  ISR_Servo.setPosition(servoHandle, 90);  // weird there was no init/failsafe?
   //Serial.println(ISR_Servo.getPulseWidth(servoHandle));
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/index.html", "text/html");
+    request->send(LittleFS, "/index.html", String(), false, processor);
   });
 
   server.on("/stop", HTTP_GET, [](AsyncWebServerRequest *request) {
-    WasherState = IDLE;  //  probably need some sort of locking or dropbox since this is async...  ish.  heavy on the ish.
+    setWasherState(rtcData, IDLE);
     request->redirect("/");
   });
 
   server.on("/spin_start", HTTP_GET, [](AsyncWebServerRequest *request) {
-    WasherState = SPINNING;  //  probably need some sort of locking or dropbox since this is async...  ish.  heavy on the ish.
+    setWasherState(rtcData, SPINNING);
     ts_spinning = millis();
     request->redirect("/");
   });
 
   server.on("/wash_start", HTTP_GET, [](AsyncWebServerRequest *request) {
-    WasherState = OSCILLATING;  //  probably need some sort of locking or dropbox since this is async...  ish.  heavy on the ish.
+    setWasherState(rtcData, OSCILLATING);
     osc_pattern_idx = 0;
     ts_oscillating = osc_last = millis();
     request->redirect("/");
@@ -130,9 +154,7 @@ void setup() {
   server.begin();
 }
 
-#define TIME_POWERDELAY (20 * 1000)
-#define TIME_OSCILLATE (5 * 60 * 1000)
-#define TIME_SPIN (3 * 60 * 1000)
+
 
 void check_and_set(int handle, int pos) {
   if (ISR_Servo.getPosition(handle) != pos) {
@@ -141,11 +163,10 @@ void check_and_set(int handle, int pos) {
 }
 
 void loop() {
-  switch (WasherState) {
+  switch (rtcData->WasherState) {
     case POWERUP:
-      //  wait 5 seconds and then start oscillating
       if (millis() - ts_powerup > (TIME_POWERDELAY)) {
-        WasherState = OSCILLATING;
+        setWasherState(rtcData, OSCILLATING);
         osc_pattern_idx = 0;
         ts_oscillating = osc_last = millis();
         //  alright I already don't like how this code is looking but whatever
@@ -154,7 +175,7 @@ void loop() {
     case OSCILLATING:
       {
         if (millis() - ts_oscillating > (TIME_OSCILLATE)) {
-          WasherState = IDLE;
+          setWasherState(rtcData, IDLE);
         }
         if (millis() - osc_last > 1000) {
           osc_last = millis();
@@ -169,8 +190,7 @@ void loop() {
     case SPINNING:
       {
         if (millis() - ts_spinning > (TIME_SPIN)) {
-
-          WasherState = IDLE;
+          setWasherState(rtcData, IDLE);
         } else if (millis() - ts_spinning > (1000)) {
           check_and_set(servoHandle, 180);
         } else {
